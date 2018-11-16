acefc873d38ad0d871ec1f22ecf80df4f195accd
Cristian Adrián Ontivero <cristianontivero@gmail.com>
2018-11-15 21:54:28 -0300

    Add Semigroup and Monoid instances for RE

--- Regexp.hs
+++ Regexp.hs
@@ -1,19 +1,30 @@
 module Regexp where
 
-data RE = Nil
-        | Bot
-        | C Char
-        | Con RE RE
-        | Alt RE RE
-        | Kle RE
+import Data.Semigroup
+import Data.Monoid (Monoid)
+
+data RE = Nil       -- ^ Empty string
+        | Bot       -- ^ Bottom
+        | C Char    -- ^ A character literal
+        | Con RE RE -- ^ Concatenation: rs
+        | Alt RE RE -- ^ Alternation: r | s
+        | Kle RE    -- ^ Kleene closure: r*
         -- | Fun2 (RE -> RE -> Bool) RE RE
         -- | Fun1 (RE -> Bool) RE
   deriving (Show, Eq, Ord)
 
+instance Semigroup RE where
+  Nil <> r = r
+  r <> Nil = r
+  r <> s   = Con r s
+
+instance Monoid RE where
+  mempty  = Nil
+
 acceptsEmptyStr :: RE -> Bool
-acceptsEmptyStr (C _)       = False
-acceptsEmptyStr Bot         = False
-acceptsEmptyStr Nil         = True
-acceptsEmptyStr (Kle _)     = True
+acceptsEmptyStr (C _)     = False
+acceptsEmptyStr Bot       = False
+acceptsEmptyStr Nil       = True
+acceptsEmptyStr (Kle _)   = True
 acceptsEmptyStr (Alt r s) = acceptsEmptyStr r || acceptsEmptyStr s
 acceptsEmptyStr (Con r s) = acceptsEmptyStr r && acceptsEmptyStr s
